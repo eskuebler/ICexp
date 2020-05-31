2df7b885c9ad1145f755c2b7f42a9002ddcf3df9
mfeyerab <mfeyerab@uwo.ca>
2020-05-31 16:49:31 -0400

    Add files via upload

---
+++ low level functions/Michelle_median_isi.m
@@ -0,0 +1,15 @@
+function output = Michelle_median_isi(LP)
+isis = [];
+k = [];
+
+for k = 1:length(LP.stats)
+   if  isfield(LP.stats{k, 1},'spTimes') 
+    if LP.stats{k, 1}.spTimes > 0
+       isis = [isis, LP.stats{k, 1}.ISI];
+    end
+   end
+end
+
+output = 1000/(median(isis));
+
+close
