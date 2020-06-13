9bca5cc3552eaf687bb592bac2cd52d1bb9a1389
mfeyerab <mfeyerab@uwo.ca>
2020-06-13 18:08:05 -0400

    Update getSubthresholdStats.m

--- low level functions/getSubthresholdStats.m
+++ low level functions/getSubthresholdStats.m
@@ -1,12 +1,11 @@
-% collect subthreshold data
-
-subamp(n,1) = round(double(a.LP.stats{k,1}.subSweepAmps),2);
-submin(n,1) = round(double(a.LP.stats{k,1}.minV),2);
+IC.subamp(n,1) = round(double(a.LP.stats{k,1}.subSweepAmps),2);
+IC.submin(n,1) = round(double(a.LP.stats{k,1}.minV),2);
 rebound_slope(n,1) = round(double(a.LP.stats{k,1}.reboundSlope),2);
 rebound_depolarization(n,1) = round(double(a.LP.stats{k,1}.reboundDepolarization),2);
-sag(n,1) = round(double(a.LP.stats{k,1}.sag),2);
-steadystate(n,1) = round(double(a.LP.stats{k,1}.subSteadyState),2);
-sag_ratio(n,1) = round(double(a.LP.stats{k,1}.sagRatio),2);
+IC.sag(n,1) = round(double(a.LP.stats{k,1}.sag),2);
+IC.steadystate(n,1) = round(double(a.LP.stats{k,1}.subSteadyState),2);
+IC.sag_ratio(n,1) = double(a.LP.stats{k,1}.sagRatio); % Michelle change
+IC.Vrest_sag_sweep(n,1) = double(a.LP.stats{k, 1}.qc.restVPre);  % Michelle change get Vm prestim of sweep for sag ratio
 if sum(isnan(a.LP.stats{k,1}.reboundAPs))==0
     nb_rebound_sp(n,1) = length(a.LP.stats{k,1}.reboundAPs);
 else
